37ac36c4645c9050c5a12762302ccfb750b901ee
falfonzo <franciscoj.anguloa@gmail.com>
2026-09-21 10:54:13 +0100

    Modified the credentials fixture to use named tuples instead of single tuple

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import os
+from typing import NamedTuple
 
 import pytest
 from dotenv import load_dotenv
@@ -9,22 +10,25 @@ from httpx_clients.dummyjson_base import DummyJsonBase
 load_dotenv()
 
 
+class Credentials(NamedTuple):
+    username: str
+    password: str
+
+
 @pytest.fixture(scope="session")
 def dummyjson_client():
-    """Creates  an instance of the DummyJsonClient class.
+    """Creates  an instance of the DummyJsonBase class.
 
-    The instance of the DummyJsonClient holds inside it access to all the client modules such as authentication,
+    The instance of the DummyJsonBase holds inside it access to all the client modules such as authentication,
     products and more all consolidated inside one  single client.
 
     Scope:  Session Scope to provide a single client for the  whole  session.
 
     Examples:
-        client.auth_client.authenticate
-        client.products_client.get_products
+        dummyjson_client.auth_client.authenticate
+        dummyjson_client.products_client.get_all_products
 
-    Yields: client (DummyJsonClient)
-
-    Notes: Upon yield, closes the client.
+    Yields: client (DummyJsonBase)
     """
     # Initialize Client (whole)
     client = DummyJsonBase()
@@ -36,20 +40,31 @@ def dummyjson_client():
 
 
 @pytest.fixture(scope="session")
-def auth_token(dummyjson_client):
+def auth_token(dummyjson_client, default_user_credentials):
     """Retrieves the authorization token from the dummy client and passes it to the test.
 
     Scope:  Session Scope considering that a single token should last up to 60 minutes.
 
     Returns: (str) Authorization token string
     """
-    auth_token = dummyjson_client.auth_client.retrieve_auth_token(username=os.environ["DEFAULT_USER_NAME"],
-                                                                  password=os.environ["DEFAULT_USER_PASS"])
+    auth_token = dummyjson_client.auth_client.retrieve_auth_token(username=default_user_credentials.username,
+                                                                  password=default_user_credentials.password)
     return auth_token
 
 
 @pytest.fixture(scope="session")
 def default_user_credentials():
-    username = os.environ["DEFAULT_USER_NAME"]
-    password = os.environ["DEFAULT_USER_PASS"]
-    return username, password
+    """Provides credentials of the default user.
+
+    Makes use of a namedTuple to  provide the username and password for the default user.
+
+    Returns:
+        credentials: namedTuple object containing the username and password of the default user.
+
+    Examples:
+        default_user_credentials.username, default_user_credentials.password
+    """
+
+    credentials = Credentials(username=os.environ["DEFAULT_USER_NAME"],
+                              password=os.environ["DEFAULT_USER_PASS"])
+    return credentials
--- tests/test_login.py
+++ tests/test_login.py
@@ -10,9 +10,8 @@ logger = _logger(__name__)
 
 
 def test_login(dummyjson_client,  default_user_credentials):
-    username,  password = default_user_credentials
-    response = dummyjson_client.auth_client.authenticate(username=username,
-                                                         password=password)
+    response = dummyjson_client.auth_client.authenticate(username=default_user_credentials.username,
+                                                         password=default_user_credentials.password)
     # Assertions using Assertion Helpers
     assert_status_code(response, 200)
     json_response_body = assert_json_response(response)
@@ -21,13 +20,12 @@ def test_login(dummyjson_client,  default_user_credentials):
     schema_validation = AuthSchema.model_validate(json_response_body)
     #  Assert logged user
     logger.debug("TEST: Validating logged user Schema against Model")
-    assert schema_validation.username == username
+    assert schema_validation.username == default_user_credentials.username
 
 
 def test_retrieve_token(dummyjson_client, default_user_credentials):
-    username,  password = default_user_credentials
-    auth_token = dummyjson_client.auth_client.retrieve_auth_token(username=username,
-                                                                  password=password)
+    auth_token = dummyjson_client.auth_client.retrieve_auth_token(username=default_user_credentials.username,
+                                                                  password=default_user_credentials.password)
     logger.debug("TEST: Assert the Token is a String")
     assert isinstance(auth_token, str), f"TEST FAIL: Auth Token is not a String. Got : {type(auth_token)}"
     logger.debug("TEST: Validating the Token is not an emtpy string")
@@ -36,12 +34,11 @@ def test_retrieve_token(dummyjson_client, default_user_credentials):
 
 
 def test_auth_token_request_time_and_expires_setters(dummyjson_client, default_user_credentials):
-    username,  password = default_user_credentials
     expiration_time = 25
     logger.debug("TEST: Setting initial timer")
     initial_time = time.time()
-    dummyjson_client.auth_client.retrieve_auth_token(username=username,
-                                                     password=password,
+    dummyjson_client.auth_client.retrieve_auth_token(username=default_user_credentials.username,
+                                                     password=default_user_credentials.password,
                                                      expires_in_mins=expiration_time)
     logger.debug("TEST: Setting final timer")
     final_time = time.time()
@@ -57,29 +54,26 @@ def test_auth_token_request_time_and_expires_setters(dummyjson_client, default_u
 
 
 def test_negative_authenticate_invalid_user(dummyjson_client, default_user_credentials):
-    username,  password = default_user_credentials
-    response = dummyjson_client.auth_client.authenticate(username=username+"1",
-                                                         password=password)
+    response = dummyjson_client.auth_client.authenticate(username=default_user_credentials.username+"1",
+                                                         password=default_user_credentials.password)
     assert_status_code(response, 400)
     json_response_body = assert_json_response(response)
     assert json_response_body == {"message": "Invalid credentials"}
 
 
 def test_negative_retrieve_auth_token_invalid_user(dummyjson_client, default_user_credentials):
-    username,  password = default_user_credentials
     with pytest.raises(httpx.HTTPStatusError) as request_error_wrapper:
-        dummyjson_client.auth_client.retrieve_auth_token(username=username+"1",
-                                                         password=password)
+        dummyjson_client.auth_client.retrieve_auth_token(username=default_user_credentials.username+"1",
+                                                         password=default_user_credentials.password)
     assert_status_code(request_error_wrapper.value.response, 400)
     assert request_error_wrapper.value.response.json() == {"message": "Invalid credentials"}, \
         f"TEST FAIL: Error message does not match: {request_error_wrapper.value.response.json()}"
 
 
 def test_negative_retrieve_auth_token_invalid_password(dummyjson_client, default_user_credentials):
-    username,  password = default_user_credentials
     with pytest.raises(httpx.HTTPStatusError) as request_error_wrapper:
-        dummyjson_client.auth_client.retrieve_auth_token(username=username,
-                                                         password=password+"1")
+        dummyjson_client.auth_client.retrieve_auth_token(username=default_user_credentials.username,
+                                                         password=default_user_credentials.password+"1")
     assert_status_code(request_error_wrapper.value.response, 400)
     assert request_error_wrapper.value.response.json() == {"message": "Invalid credentials"}, \
         f"TEST FAIL: Error message does not match: {request_error_wrapper.value.response.json()}"
